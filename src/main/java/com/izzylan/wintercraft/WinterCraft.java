766e31293b7a25decb63b66c59f2cd7167965bd1
Izzy <avrisaac555@gmail.com>
2020-02-08 14:25:10 -0800

    Made names constants

--- src/main/java/com/izzylan/wintercraft/WinterCraft.java
+++ src/main/java/com/izzylan/wintercraft/WinterCraft.java
@@ -12,15 +12,18 @@ import net.minecraft.util.registry.Registry;
 
 public class WinterCraft implements ModInitializer {
 
+    public static final String NAMESPACE = "wintercraft";
+
+    public static final String ID_PERMAFROST = "permafrost";
     public static final Block BLOCK_PERMAFROST = new Block(FabricBlockSettings.of(Material.EARTH).build());
 
     public static Identifier BuildModIdentifier(String name) {
-        return new Identifier("wintercraft", name);
+        return new Identifier(NAMESPACE, name);
     }
 
     @Override
     public void onInitialize() {
-        Registry.register(Registry.BLOCK, BuildModIdentifier("Permafrost"), BLOCK_PERMAFROST);
-        Registry.register(Registry.ITEM, BuildModIdentifier("Permafrost"), new BlockItem(BLOCK_PERMAFROST, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+        Registry.register(Registry.BLOCK, BuildModIdentifier(ID_PERMAFROST), BLOCK_PERMAFROST);
+        Registry.register(Registry.ITEM, BuildModIdentifier(ID_PERMAFROST), new BlockItem(BLOCK_PERMAFROST, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
     }
 }
