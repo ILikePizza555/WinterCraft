6bc304511a68132c4b0efe02c86db55cdb8e1fde
Izzy <avrisaac555@gmail.com>
2020-02-08 14:18:18 -0800

    Added permafrost block

--- src/main/java/com/izzylan/wintercraft/WinterCraft.java
+++ src/main/java/com/izzylan/wintercraft/WinterCraft.java
@@ -1,10 +1,26 @@
 package com.izzylan.wintercraft;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class WinterCraft implements ModInitializer {
+
+    public static final Block BLOCK_PERMAFROST = new Block(FabricBlockSettings.of(Material.EARTH).build());
+
+    public static Identifier BuildModIdentifier(String name) {
+        return new Identifier("wintercraft", name);
+    }
+
     @Override
     public void onInitialize() {
-
+        Registry.register(Registry.BLOCK, BuildModIdentifier("Permafrost"), BLOCK_PERMAFROST);
+        Registry.register(Registry.ITEM, BuildModIdentifier("Permafrost"), new BlockItem(BLOCK_PERMAFROST, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
     }
 }
